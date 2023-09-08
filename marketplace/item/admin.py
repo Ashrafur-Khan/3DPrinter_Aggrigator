1479c13ffc9e6940e268d747711da55e6b5c8d98
Ashrafur <ashrafur2203@gmail.com>
2023-09-08 01:24:52 -0400

    adding Items model and more categories

--- marketplace/item/admin.py
+++ marketplace/item/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Item
+admin.site.register(Item)
 admin.site.register(Category)
--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -10,3 +11,22 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Item(models.Model): 
+    #when deleting a category, all items will be deleted
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null = True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    #if user is deleted, all items will also be deleted
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ('name', )
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        return self.name 
